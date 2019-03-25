4bf7621332c5926fdf9be45b7e9d4cbb4931dc7c
nicole <nikybg96@gmail.com>
2019-03-25 17:10:01 -0400

    Test: get bill with name and total salary with discount and contributions of employee

--- src/__tests__/bill_test.js
+++ src/__tests__/bill_test.js
@@ -2,7 +2,15 @@ import {generateBill} from '../bill'
 
 describe ("Bill", function(){
     test("get bill with name and salary of employee", function () {
-        let bill = generateBill("Andres", 500);
-        expect(bill).toBe("Bill \n Employee: Andres \n Salary: 500");
+        let bill = generateBill("Andres", 500, 0, 0);
+        expect(bill).toBe("Bill \n Employee: Andres \n Basic Salary: 500 \n Discount: 0 \n Contributions: 0 \n Total Salary: 500");
+    });
+    test("get bill with name and salary with discount of employee", function () {
+        let bill = generateBill("Andres", 500, 50, 0);
+        expect(bill).toBe("Bill \n Employee: Andres \n Basic Salary: 500 \n Discount: 50 \n Contributions: 0 \n Total Salary: 250");
+    });
+    test("get bill with name and total salary with discount and contributions of employee", function () {
+        let bill = generateBill("Andres", 500, 50, 100);
+        expect(bill).toBe("Bill \n Employee: Andres \n Basic Salary: 500 \n Discount: 50 \n Contributions: 100 \n Total Salary: 150");
     });
 })
--- src/bill.js
+++ src/bill.js
@@ -1,3 +1,5 @@
-export function generateBill(employeeName, employeeSalary) {
-    return "Bill \n Employee: " + employeeName + " \n Salary: " + employeeSalary; 
+export function generateBill(employeeName, employeeSalary, discount, contributions) {
+    let totalSalary = employeeSalary - (employeeSalary * discount /100);
+    totalSalary = totalSalary - contributions;
+    return "Bill \n Employee: " + employeeName + " \n Basic Salary: " + employeeSalary + " \n Discount: " + discount + " \n Contributions: " + contributions + " \n Total Salary: " + totalSalary; 
 }
