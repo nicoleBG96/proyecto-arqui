babee5ad5bb596388e7d9ab99de83fd6013850cd
tefi1508 <tefi1508@gmail.com>
2019-03-28 10:07:11 -0400

    get bill with name and salary of full-time employee with 10 percent discount

--- src/__tests__/bill_test.js
+++ src/__tests__/bill_test.js
@@ -9,6 +9,14 @@ describe ("Bill", function(){
         let bill = generateBill(employee);
         expect(bill).toBe("Bill \n Employee: Royer Torrico \n Basic Salary: 1100\n Discount: 0% \n Total Discount: 0\n Total Salary: 1100");
     });
+
+    test("get bill with name and salary of full-time employee with discount of 10 percent", function () {
+        let salaryCalculator = new FullTimeEmployeeCalculator(1100,10);
+        let employee = new Employee("Royer Torrico", 1, 2, "Champion",salaryCalculator);
+        let bill = generateBill(employee);
+        expect(bill).toBe("Bill \n Employee: Royer Torrico \n Basic Salary: 1100\n Discount: 10% \n Total Discount: 110\n Total Salary: 990");
+    });
+
     test("get bill with name and salary of half-time employee", function () {
         let salaryCalculator = new HalfTimeEmployeeCalculator(100,11);
         let employee = new Employee("Royer Torrico", 1, 2, "Champion",salaryCalculator);
