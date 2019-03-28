c9e758e21791573c06e1440cbe4da291debe87e4
tefi1508 <tefi1508@gmail.com>
2019-03-28 09:37:28 -0400

    half-time employee

--- src/__tests__/bill_test.js
+++ src/__tests__/bill_test.js
@@ -1,6 +1,6 @@
 import {generateBill} from '../bill'
 import {Employee} from '../Employee'
-import {FullTimeEmployeeCalculator} from '../SalaryCalculator'
+import {FullTimeEmployeeCalculator, HalfTimeEmployeeCalculator} from '../SalaryCalculator'
 
 describe ("Bill", function(){
     test("get bill with name and salary of full-time employee", function () {
@@ -9,5 +9,10 @@ describe ("Bill", function(){
         let bill = generateBill(employee);
         expect(bill).toBe("Bill \n Employee: Royer Torrico \n Total Salary: 1100");
     });
-    
+    test("get bill with name and salary of half-time employee", function () {
+        let salaryCalculator = new HalfTimeEmployeeCalculator(100,11);
+        let employee = new Employee("Royer Torrico", 1, 2, "Champion",salaryCalculator);
+        let bill = generateBill(employee);
+        expect(bill).toBe("Bill \n Employee: Royer Torrico \n Total Salary: 1100");
+    });
 })
