9c1026014b13555340a3492c6a5f005f518e70f9
3aVarana <3av.arana@gmail.com>
2019-04-08 21:34:39 -0400

    dada la fecha 31/05/2019 deberia devolverme que es el ultimo dia habil del mes

---
+++ src/__tests__/ComprobanteFechaDePago.js
@@ -0,0 +1,9 @@
+import {ComprobanteFechaDePagoEmpleadoFijo, ComprobanteFechaDePagoEmpleadoParcial, ComprobanteFechaDePagoVendedor} from '../Comprobantes/ComprobanteFechaDePago';
+
+describe ("ComprobanteFechaDePago", function(){
+    test("dada la fecha 31/05/2019 deberia devolverme que es el ultimo dia habil del mes", function () {
+        let comprobanteFechaDePagoEmpleadoFijo = new ComprobanteFechaDePagoEmpleadoFijo();
+        let fecha = new Date('5, 31, 2019');
+        expect(comprobanteFechaDePagoEmpleadoFijo.esFechaDePago(fecha)).toBe(true);
+    });
+})
