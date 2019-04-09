e97685fa4f6f389f868ca349fdb79a14d126526a
3aVarana <3av.arana@gmail.com>
2019-04-08 21:16:51 -0400

    Dado un empleado fijo que gana 1100, al cual se le descuente un 10% calcular su salario a pagar

--- src/__tests__/Empleado_text.js
+++ src/__tests__/Empleado_text.js
@@ -8,12 +8,20 @@ import { Ventas } from '../Comision/Ventas';
 import { TarjetaDeVenta } from '../Comision/TarjetaDeVenta';
 
 describe ("Empleado", function(){
-    test("dado un empleado fijo que gana 1100 calcular su salario", function () {
+    test("dado un empleado fijo que gana 1100 calcular su salario a pagar", function () {
         let comprobanteFechaDePagoEmpleadoFijo = new ComprobanteFechaDePagoEmpleadoFijo();
         let calculadoraDeSalario = new CalculadoraEmpleadoFijo(comprobanteFechaDePagoEmpleadoFijo, 1100,0, 0);
         let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
         let fecha = new Date('5, 31, 2019');
         expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Basico: 1100\n Descuento: 0% \n Descuento Total: 0\n Aportes: 0%\n Total Aportes: 0\n Salario Total: 1100");
+    });
+
+    test("dado un empleado fijo que gana 1100, al cual se le descuente un 10% calcular su salario a pagar", function () {
+        let comprobanteFechaDePagoEmpleadoFijo = new ComprobanteFechaDePagoEmpleadoFijo();
+        let calculadoraDeSalario = new CalculadoraEmpleadoFijo(comprobanteFechaDePagoEmpleadoFijo, 1100,10, 0);
+        let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
+        let fecha = new Date('5, 31, 2019');
+        expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Basico: 1100\n Descuento: 10% \n Descuento Total: 110\n Aportes: 0%\n Total Aportes: 0\n Salario Total: 990");
     });
 
 })
