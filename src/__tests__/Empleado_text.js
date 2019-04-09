79350b17d35acbe82a5887957f3420ffa4c82ba9
3aVarana <3av.arana@gmail.com>
2019-04-08 21:21:37 -0400

    dada un empleado parcial que gana 100 por hora y no tiene horas trabajadadas, deberia ganar 0

--- src/__tests__/Empleado_text.js
+++ src/__tests__/Empleado_text.js
@@ -8,7 +8,7 @@ import { Ventas } from '../Comision/Ventas';
 import { TarjetaDeVenta } from '../Comision/TarjetaDeVenta';
 
 describe ("Empleado", function(){
-    test("dado un empleado fijo que gana 1100 calcular su salario a pagar", function () {
+    test("dado un empleado fijo que gana 1100, deberia ganar 1100", function () {
         let comprobanteFechaDePagoEmpleadoFijo = new ComprobanteFechaDePagoEmpleadoFijo();
         let calculadoraDeSalario = new CalculadoraEmpleadoFijo(comprobanteFechaDePagoEmpleadoFijo, 1100,0, 0);
         let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
@@ -16,7 +16,7 @@ describe ("Empleado", function(){
         expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Basico: 1100\n Descuento: 0% \n Descuento Total: 0\n Aportes: 0%\n Total Aportes: 0\n Salario Total: 1100");
     });
 
-    test("dado un empleado fijo que gana 1100, al cual se le descuente un 10% calcular su salario a pagar", function () {
+    test("dado un empleado fijo que gana 1100, al cual se le descuenta un 10% deberia ganar 990", function () {
         let comprobanteFechaDePagoEmpleadoFijo = new ComprobanteFechaDePagoEmpleadoFijo();
         let calculadoraDeSalario = new CalculadoraEmpleadoFijo(comprobanteFechaDePagoEmpleadoFijo, 1100,10, 0);
         let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
@@ -24,7 +24,7 @@ describe ("Empleado", function(){
         expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Basico: 1100\n Descuento: 10% \n Descuento Total: 110\n Aportes: 0%\n Total Aportes: 0\n Salario Total: 990");
     });
 
-    test("dado un empleado fijo que gana 1100, al cual se le descuente un 10% y el cual aporta 20% calcular su salario a pagar", function () {
+    test("dado un empleado fijo que gana 1100, al cual se le descuente un 10% y el cual aporta 20% deberia ganar 770", function () {
         let comprobanteFechaDePagoEmpleadoFijo = new ComprobanteFechaDePagoEmpleadoFijo();
         let calculadoraDeSalario = new CalculadoraEmpleadoFijo(comprobanteFechaDePagoEmpleadoFijo, 1100,10, 20);
         let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
@@ -32,4 +32,12 @@ describe ("Empleado", function(){
         expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Basico: 1100\n Descuento: 10% \n Descuento Total: 110\n Aportes: 20%\n Total Aportes: 220\n Salario Total: 770");
     });
 
+    test("dada un empleado parcial que gana 100 por hora y no tiene horas trabajadadas, deberia ganar 0", function () {
+        let comprobanteFechaDePagoEmpleadoParcial = new ComprobanteFechaDePagoEmpleadoParcial();
+        let tarjetaDeAsistencia = new TarjetaDeAsistencia();
+        let calculadoraDeSalario = new CalculadoraEmpleadoParcial(comprobanteFechaDePagoEmpleadoParcial, 100, tarjetaDeAsistencia);
+        let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
+        let fecha = new Date('5, 24, 2019');
+        expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Total: 0");
+    });
 })
