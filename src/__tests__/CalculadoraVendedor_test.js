dc0ec7efb5b47bb5a23edb3c4d18e873ca468eb2
tefi1508 <tefi1508@gmail.com>
2019-04-09 09:12:23 -0400

    Test CalculadoraVendedor SVR

---
+++ src/__tests__/CalculadoraVendedor_test.js
@@ -0,0 +1,14 @@
+import {CalculadoraVendedor} from '../Calculadoras/CalculadoraDeSalario'
+import {ComprobanteFechaDePagoVendedor} from '../Comprobantes/ComprobanteFechaDePago'
+import { TarjetaDeVenta } from '../Comision/TarjetaDeVenta';
+import { Ventas } from '../Comision/Ventas';
+
+describe ("Calculadora Vendedor", function(){
+    test("dado un salario basico de 2500, sin ventas realizadas, el salario total es de 2500", function () {
+        let comprobanteFechaDePagoVendedor = new ComprobanteFechaDePagoVendedor();
+        let ventas = new Ventas();
+        let calculadoraDeSalario = new CalculadoraVendedor(comprobanteFechaDePagoVendedor, 2500, ventas);
+        let fecha = new Date('5, 24, 2019');
+        expect(calculadoraDeSalario.calcularSalario(fecha)).toBe("Salario Total: 2500");
+    });
+})
