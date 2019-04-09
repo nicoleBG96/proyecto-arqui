cd33dfe5a859615c88c3ed99a2bd03471b31529c
tefi1508 <tefi1508@gmail.com>
2019-04-09 09:35:18 -0400

    Test Ventas con 2 ventas registradas

--- src/__tests__/Ventas_test.js
+++ src/__tests__/Ventas_test.js
@@ -14,4 +14,14 @@ describe ("TarjetaDeAsistencia", function(){
         ventas.registrarVenta(tarjetaDeVenta1);
         expect(ventas.calcularTotalDeComisiones()).toBe(500);
     });
+
+    test("dado que existen 2 ventas registradas, deberia devolver que la comision total es igual a 1200", function () {
+        let ventas = new Ventas();
+        let fecha = new Date('5, 10, 2019');
+        let tarjetaDeVenta1 = new TarjetaDeVenta(fecha,2000,50);
+        let tarjetaDeVenta2 = new TarjetaDeVenta(fecha,200,100);
+        ventas.registrarVenta(tarjetaDeVenta1);
+        ventas.registrarVenta(tarjetaDeVenta2);
+        expect(ventas.calcularTotalDeComisiones()).toBe(1200);
+    });
 })
