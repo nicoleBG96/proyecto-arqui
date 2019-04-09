d593e97482dda920c5024c57bef7755500caf533
3aVarana <3av.arana@gmail.com>
2019-04-08 23:32:56 -0400

    Dado que hay una terjeta de asistencia de 8 a 16 deberia devolver 8 horas trabajadas

--- src/__tests__/TarjetaDeAsistencia_test.js
+++ src/__tests__/TarjetaDeAsistencia_test.js
@@ -6,4 +6,13 @@ describe ("TarjetaDeAsistencia", function(){
         let tarjetaDeAsistencia = new TarjetaDeAsistencia()
         expect(tarjetaDeAsistencia.calcularHorasTrabajadas()).toBe(0);
     });
+
+    test("dado que hay una tarjeta de asintencia de 8 a 16 deberia devolver 8 horas trabajadas en total", function () {
+        let asitenciaDiaria = new AsistenciaDiaria("29/01/2019", 8, 16)
+        let tarjetaDeAsistencia = new TarjetaDeAsistencia()
+        tarjetaDeAsistencia.registrarAsistencia(asitenciaDiaria);
+        expect(tarjetaDeAsistencia.calcularHorasTrabajadas()).toBe(8);
+    });
+
+    
 })
