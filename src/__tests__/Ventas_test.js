469a31da719ac6adc350a0f89deccb2e9094b083
tefi1508 <tefi1508@gmail.com>
2019-04-09 09:33:25 -0400

    Test Ventas con 1 venta registrada

--- src/__tests__/Ventas_test.js
+++ src/__tests__/Ventas_test.js
@@ -1,9 +1,17 @@
 import { Ventas } from '../Comision/Ventas';
-import { TarjetaDeVentas } from '../Comision/TarjetaDeVenta'
+import { TarjetaDeVenta } from '../Comision/TarjetaDeVenta'
 
 describe ("TarjetaDeAsistencia", function(){
     test("dado que no existen ventas registradas, deberia devolver que la comision total es igual a 0", function () {
         let ventas = new Ventas()
         expect(ventas.calcularTotalDeComisiones()).toBe(0);
+    });
+
+    test("dado que existe 1 venta registradas, deberia devolver que la comision total es igual a 500", function () {
+        let ventas = new Ventas();
+        let fecha = new Date('5, 10, 2019');
+        let tarjetaDeVenta1 = new TarjetaDeVenta(fecha,1000,50);
+        ventas.registrarVenta(tarjetaDeVenta1);
+        expect(ventas.calcularTotalDeComisiones()).toBe(500);
     });
 })
