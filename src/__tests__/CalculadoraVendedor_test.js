ce77536605888f5194ecaed4e3cc94676ceeaf7e
tefi1508 <tefi1508@gmail.com>
2019-04-09 09:16:58 -0400

    Test CalculadoraVendedor con 3 ventas realizadas

--- src/__tests__/CalculadoraVendedor_test.js
+++ src/__tests__/CalculadoraVendedor_test.js
@@ -12,7 +12,7 @@ describe ("Calculadora Vendedor", function(){
         expect(calculadoraDeSalario.calcularSalario(fecha)).toBe("Salario Total: 2500");
     });
 
-    test("dado un salario basico de 3000, sin ventas realizadas, el salario total es de 3500", function () {
+    test("dado un salario basico de 3000, con una venta realizada, el salario total es de 3500", function () {
         let comprobanteFechaDePagoVendedor = new ComprobanteFechaDePagoVendedor();
         let ventas = new Ventas();
         let tarjetaDeVenta1 = new TarjetaDeVenta("05/03/2019",1000, 50);
@@ -20,5 +20,19 @@ describe ("Calculadora Vendedor", function(){
         let calculadoraDeSalario = new CalculadoraVendedor(comprobanteFechaDePagoVendedor, 3000, ventas);
         let fecha = new Date('5, 24, 2019');
         expect(calculadoraDeSalario.calcularSalario(fecha)).toBe("Salario Total: 3500");
+    });
+
+    test("dado un salario basico de 2500, con 3 ventas realizadas, el salario total es de 3250", function () {
+        let comprobanteFechaDePagoVendedor = new ComprobanteFechaDePagoVendedor();
+        let ventas = new Ventas();
+        let tarjetaDeVenta1 = new TarjetaDeVenta("05/03/2019",1000, 50);
+        let tarjetaDeVenta2 = new TarjetaDeVenta("05/03/2019",1000, 25);
+        let tarjetaDeVenta3 = new TarjetaDeVenta("05/03/2019",600, 0);
+        ventas.registrarVenta(tarjetaDeVenta1);
+        ventas.registrarVenta(tarjetaDeVenta2);
+        ventas.registrarVenta(tarjetaDeVenta3);
+        let calculadoraDeSalario = new CalculadoraVendedor(comprobanteFechaDePagoVendedor, 2500, ventas);
+        let fecha = new Date('5, 24, 2019');
+        expect(calculadoraDeSalario.calcularSalario(fecha)).toBe("Salario Total: 3250");
     });
 })
