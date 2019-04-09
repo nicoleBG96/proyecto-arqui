211bc6ee84aa72d79984145362dcc08d60a232e5
tefi1508 <tefi1508@gmail.com>
2019-04-09 09:03:19 -0400

    Test CalculadoraEmpleadoParcial con 8HT

--- src/__tests__/CalculadoraEmpleadoParcial_test.js
+++ src/__tests__/CalculadoraEmpleadoParcial_test.js
@@ -12,4 +12,14 @@ describe ("Calculadora Empleado Parcial", function(){
         expect(calculadoraDeSalario.calcularSalario(fecha)).toBe("Salario Total: 0");
     });
 
+    test("dado un salario por hora de 300, con 8 horas trabajadas, el sueldo calculado deberia ser 2400", function () {
+        let comprobanteFechaDePagoEmpleadoParcial = new ComprobanteFechaDePagoEmpleadoParcial();
+        let tarjetaDeAsistencia = new TarjetaDeAsistencia();
+        let asistenciaDiaria = new AsistenciaDiaria("30/03/2019", 8, 16);
+        tarjetaDeAsistencia.registrarAsistencia(asistenciaDiaria);
+        let calculadoraDeSalario = new CalculadoraEmpleadoParcial(comprobanteFechaDePagoEmpleadoParcial, 300, tarjetaDeAsistencia);
+        let fecha = new Date('5, 24, 2019');
+        expect(calculadoraDeSalario.calcularSalario(fecha)).toBe("Salario Total: 2400");
+    });
+
 })
