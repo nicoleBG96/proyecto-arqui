71e32e8e6724fd40b8027e3e8283af33927b3e09
3aVarana <3av.arana@gmail.com>
2019-05-14 14:41:51 -0400

    EmpleadoParcial_test fixed

--- src/__tests__/Empleado_test.js
+++ src/__tests__/Empleado_test.js
@@ -12,7 +12,7 @@ import { TarjetaVentas } from '../CalculadorasDeMontos/EmpleadoComision/TarjetaV
 import { NotificadorEmail } from '../NotificadoresDePago/NotificadorEmail';
 import { PagoPorDeposito } from '../MetodoDePago/PagoPorDeposito';
 
-describe ("Empleado", function(){
+describe("Empleado", function () {
     test("dado un empleado fijo que gana 1100 al calcular su salario deberia devolver 1100", function () {
         let calculadoraDeSalario = new CalculadoraMontosEmpleadoFijo(1100, 0, 0);
         let verificadorFechaDePagoEmpleadoFijo = new VerificadorFechaDePagoEmpleadoFijo();
@@ -40,40 +40,43 @@ describe ("Empleado", function(){
         expect(empleado.calcularSalarioAPagar()).toBe(770);
     });
 
-    // test("dada un empleado parcial que gana 100 por hora y no tiene horas trabajadadas, deberia ganar 0", function () {
-    //     let comprobanteFechaDePagoEmpleadoParcial = new ComprobanteFechaDePagoEmpleadoParcial();
-    //     let tarjetaDeAsistencia = new TarjetaDeAsistencia();
-    //     let calculadoraDeSalario = new CalculadoraEmpleadoParcial(comprobanteFechaDePagoEmpleadoParcial, 100, tarjetaDeAsistencia);
-    //     let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
-    //     let fecha = new Date('5, 24, 2019');
-    //     expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Total: 0");
-    // });
+    test("dada un empleado parcial que gana 100 por hora y no tiene horas trabajadadas, al calcular su salariodeberia ganar devolver 0", function () {
+        let tarjetaDeAsistencias = new TarjetaDeAsistencias();
+        let calculadoraDeSalario = new CalculadoraMontosEmpleadoParcial(100, tarjetaDeAsistencias);
+        let verificadorFechaDePagoEmpleadoFijo = new VerificadorFechaDePagoEmpleadoParcial();
+        let notificador = new NotificadorEmail();
+        let metodoDePago = new PagoPorDeposito();
+        let empleado = new Empleado("Royer Torrico", "T-123", 8798415, "Champion", calculadoraDeSalario, verificadorFechaDePagoEmpleadoFijo, notificador, metodoDePago);
+        expect(empleado.calcularSalarioAPagar()).toBe(0);
+    });
 
-    // test("dada un empleado parcial que gana 100 por hora y tiene 3 horas trabajadadas, deberia ganar 300", function () {
-    //     let comprobanteFechaDePagoEmpleadoParcial = new ComprobanteFechaDePagoEmpleadoParcial();
-    //     let tarjetaDeAsistencia = new TarjetaDeAsistencia();
-    //     let asistenciaDiaria = new AsistenciaDiaria("30/03/2019", 9, 12);
-    //     tarjetaDeAsistencia.registrarAsistencia(asistenciaDiaria);
-    //     let calculadoraDeSalario = new CalculadoraEmpleadoParcial(comprobanteFechaDePagoEmpleadoParcial, 100, tarjetaDeAsistencia);
-    //     let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
-    //     let fecha = new Date('5, 24, 2019');
-    //     expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Total: 300");
-    // });
+    test("dada un empleado parcial que gana 100 por hora y tiene 3 horas trabajadadas, al calcular su salariodeberia ganar devolver 300", function () {
+        let tarjetaDeAsistencias = new TarjetaDeAsistencias();
+        let asistenciaDiaria = new AsistenciaDiaria("30/03/2019", 9, 12);
+        tarjetaDeAsistencias.registrarAsistencia(asistenciaDiaria);
+        let calculadoraDeSalario = new CalculadoraMontosEmpleadoParcial(100, tarjetaDeAsistencias);
+        let verificadorFechaDePagoEmpleadoFijo = new VerificadorFechaDePagoEmpleadoParcial();
+        let notificador = new NotificadorEmail();
+        let metodoDePago = new PagoPorDeposito();
+        let empleado = new Empleado("Royer Torrico", "T-123", 8798415, "Champion", calculadoraDeSalario, verificadorFechaDePagoEmpleadoFijo, notificador, metodoDePago);
+        expect(empleado.calcularSalarioAPagar()).toBe(300);
+    });
 
-    // test("dada un empleado parcial que gana 100 por hora y tiene 10 horas trabajadadas, deberia ganar 1000", function () {
-    //     let comprobanteFechaDePagoEmpleadoParcial = new ComprobanteFechaDePagoEmpleadoParcial();
-    //     let tarjetaDeAsistencia = new TarjetaDeAsistencia();
-    //     let asistenciaDiaria = new AsistenciaDiaria("30/03/2019", 9, 12);
-    //     tarjetaDeAsistencia.registrarAsistencia(asistenciaDiaria);
-    //     let asistenciaDiaria2 = new AsistenciaDiaria("31/03/2019", 9, 12);
-    //     tarjetaDeAsistencia.registrarAsistencia(asistenciaDiaria2);
-    //     let asistenciaDiaria3 = new AsistenciaDiaria("30/03/2019", 9, 13);
-    //     tarjetaDeAsistencia.registrarAsistencia(asistenciaDiaria3);
-    //     let calculadoraDeSalario = new CalculadoraEmpleadoParcial(comprobanteFechaDePagoEmpleadoParcial, 100, tarjetaDeAsistencia);
-    //     let empleado = new Empleado("Royer Torrico", 1, 2, "Champion",calculadoraDeSalario);
-    //     let fecha = new Date('5, 24, 2019');
-    //     expect(empleado.calcularSalarioAPagar(fecha)).toBe("Salario Total: 1000");
-    // });
+    test("dada un empleado parcial que gana 100 por hora y tiene 10 horas trabajadadas, deberia ganar 1000", function () {
+        let tarjetaDeAsistencias = new TarjetaDeAsistencias();
+        let asistenciaDiaria = new AsistenciaDiaria("30/03/2019", 9, 12);
+        tarjetaDeAsistencias.registrarAsistencia(asistenciaDiaria);
+        let asistenciaDiaria2 = new AsistenciaDiaria("31/03/2019", 9, 12);
+        tarjetaDeAsistencias.registrarAsistencia(asistenciaDiaria2);
+        let asistenciaDiaria3 = new AsistenciaDiaria("30/03/2019", 9, 13);
+        tarjetaDeAsistencias.registrarAsistencia(asistenciaDiaria3);
+        let calculadoraDeSalario = new CalculadoraMontosEmpleadoParcial(100, tarjetaDeAsistencias);
+        let verificadorFechaDePagoEmpleadoFijo = new VerificadorFechaDePagoEmpleadoParcial();
+        let notificador = new NotificadorEmail();
+        let metodoDePago = new PagoPorDeposito();
+        let empleado = new Empleado("Royer Torrico", "T-123", 8798415, "Champion", calculadoraDeSalario, verificadorFechaDePagoEmpleadoFijo, notificador, metodoDePago);
+        expect(empleado.calcularSalarioAPagar()).toBe(1000);
+    });
 
     // test("dado un empleado vendedor con sueldo basico de 100 y sin ninguna venta registrada, deberia ganar 100", function () {
     //     let comprobanteFechaDePagoVendedor = new ComprobanteFechaDePagoVendedor();
