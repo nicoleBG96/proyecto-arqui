a8fe02d00792bdf21588592d32c846a1937bdb3e
3aVarana <3av.arana@gmail.com>
2019-06-04 09:36:21 -0400

    Le projecte e finite

--- dominio/DTO/CrearEmpleadoPeticion.js
+++ dominio/DTO/CrearEmpleadoPeticion.js
@@ -1,10 +1,59 @@
 class CrearEmpleadoPeticion {
-    constructor(peticion){
+    constructor(peticion) {
         this.peticion = peticion;
     }
 
-    darFormato(){
-        return this.peticion.body;
+    removerCampos(empleado) {
+        switch (empleado.Tipo) {
+            case "Fijo":
+                delete empleado["SalarioPorHora"]
+                delete empleado["SalarioBase"]
+                break;
+            case "Parcial":
+                delete empleado["Salario"]
+                delete empleado["SalarioBase"]
+                break;
+            case "PorComision":
+                delete empleado["SalarioPorHora"]
+                delete empleado["Salario"]
+                break;
+        }
+        return empleado;
+    }
+
+    seleccionarNotificadores(empleado) {
+        if (empleado.Notificadores.NotificadorEmail === "") {
+            delete empleado.Notificadores["NotificadorEmail"]
+        }
+        if (empleado.Notificadores.NotificadorFacebook === "") {
+            delete empleado.Notificadores["NotificadorFacebook"]
+        }
+        if (empleado.Notificadores.NotificadorWhatsapp === "") {
+            delete empleado.Notificadores["NotificadorWhatsapp"]
+        }
+        return empleado;
+    }
+
+    darFormato() {
+        let fecha = new Date(this.peticion.body.FechaInicio);
+        let empleado = {
+            "Nombre": this.peticion.body.Nombre,
+            "Codigo": parseInt(this.peticion.body.Codigo),
+            "Ci": parseInt(this.peticion.body.Ci),
+            "FechaInicio": (fecha.getMonth() + 1) + ", " + fecha.getDate() + ", " + fecha.getFullYear(),
+            "Tipo": this.peticion.body.Tipo,
+            "Cargo": this.peticion.body.Cargo,
+            "MetodoDePago": this.peticion.body.MetodoDePago,
+            "Salario": this.peticion.body.Salario,
+            "SalarioPorHora": this.peticion.body.Salario,
+            "SalarioBase": this.peticion.body.Salario,
+            "Notificadores": this.peticion.body.Notificadores,
+            "Sindicato": this.peticion.body.Sindicato
+        };
+        console.log(empleado);
+        empleado = this.removerCampos(empleado);
+        empleado = this.seleccionarNotificadores(empleado);
+        return empleado;
     }
 }
 
--- dominio/DTO/RegistrarAsistenciaPeticion.js
+++ dominio/DTO/RegistrarAsistenciaPeticion.js
@@ -7,8 +7,8 @@ class RegistrarAsistenciaPeticion {
         let fecha = new Date(this.peticion.body.Fecha);
         let asistencia = {
             "Fecha" : (fecha.getMonth()+1) + ", " + fecha.getDate() + ", " + fecha.getFullYear(),
-            "HoraInicio" : parseInt(this.peticion.body.HoraInicio),
-            "HoraFinal" : parseInt(this.peticion.body.HoraFinal),
+            "HoraIngreso" : parseInt(this.peticion.body.HoraInicio),
+            "HoraSalida" : parseInt(this.peticion.body.HoraFinal),
             "CodigoEmpleado" : parseInt(this.peticion.body.CodigoEmpleado)
         };
         return asistencia;
